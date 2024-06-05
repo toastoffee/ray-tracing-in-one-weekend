c5ce11f57f16046af33b202dc15a5e15a43336f8
toastoffee <931391442@qq.com>
2024-06-05 15:13:31 +0800

    add ray.hpp

--- src/ray.hpp
+++ src/ray.hpp
@@ -13,4 +13,25 @@
 #ifndef RAY_TRACING_IN_ONE_WEEKEND_RAY_HPP
 #define RAY_TRACING_IN_ONE_WEEKEND_RAY_HPP
 
+#include "vec3.hpp"
+
+
+class Ray {
+public:
+    Point3 orig;
+    Vec3 dir;
+
+public:
+    Ray() {}
+    Ray(const Point3& origin, const Vec3& direction)
+        : orig(origin), dir(direction) {}
+
+    Point3 origin() const { return orig; }
+    Vec3 direction() const { return dir; }
+    Point3 at(double t) const {
+        return orig + t * dir;
+    }
+
+};
+
 #endif //RAY_TRACING_IN_ONE_WEEKEND_RAY_HPP

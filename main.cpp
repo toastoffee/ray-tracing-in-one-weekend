44a8bb5faf6ef5f413c35d984786ad9d216e1271
toastoffee <931391442@qq.com>
2024-06-05 15:56:52 +0800

    raytrace sphere

--- main.cpp
+++ main.cpp
@@ -4,7 +4,20 @@
 #include "src/vec3.hpp"
 #include "src/ray.hpp"
 
+bool hit_sphere(const Point3& center, double radius, const Ray& r) {
+    Vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
 Color ray_color(const Ray& r){
+    if(hit_sphere(Point3(0,0,-1), 0.5, r)){
+        return Color (1, 0, 0);
+    }
+
     Vec3 unit_direction = normalize(r.direction());
     double t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * Color(1.0, 1.0, 1.0) + t * Color(0.5, 0.7, 1.0);

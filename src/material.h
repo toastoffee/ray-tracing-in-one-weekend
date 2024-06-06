02df7f0c5943076ba84d8c37389b93515d5dc2f6
toastoffee <931391442@qq.com>
2024-06-06 16:52:16 +0800

    add snell's law

--- src/material.h
+++ src/material.h
@@ -48,19 +48,41 @@ public:
 
 class Metal : public Material {
 public:
-    Metal(const Color& a) : albedo(a) {}
+    Metal(const Color& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
     virtual bool Scatter(
             const Ray& r_in, const hit_record& rec, Color& attenuation, Ray& scattered
     ) const override {
         Vec3 reflected = reflect(normalize(r_in.direction()), rec.normal);
-        scattered = Ray(rec.p, reflected);
+        scattered = Ray(rec.p, reflected + fuzz*random_in_unit_sphere());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
 
 public:
     Color albedo;
+    double fuzz;
+};
+
+class Dielectric : public Material {
+public:
+    Dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    virtual bool Scatter(
+            const Ray& r_in, const hit_record& rec, Color& attenuation, Ray& scattered
+    ) const override {
+        attenuation = Color(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0/ir) : ir;
+
+        Vec3 unit_direction = normalize(r_in.direction());
+        Vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = Ray(rec.p, refracted);
+        return true;
+    }
+
+public:
+    double ir; // Index of Refraction
 };
 
 #endif //RAY_TRACING_IN_ONE_WEEKEND_MATERIAL_H
--- src/vec3.hpp
+++ src/vec3.hpp
@@ -149,4 +149,12 @@ Vec3 reflect(const Vec3& v, const Vec3& n) {
     return v - 2*dot(v,n)*n;
 }
 
+Vec3 refract(const Vec3& uv, const Vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    Vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    Vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.squared_length())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
+
 #endif //RAY_TRACING_IN_ONE_WEEKEND_VEC3_HPP

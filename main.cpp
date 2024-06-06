7b482f627500856559517baa0763498ed092e799
toastoffee <931391442@qq.com>
2024-06-06 18:14:06 +0800

    test a Hollow Glass Sphere

--- main.cpp
+++ main.cpp
@@ -50,6 +50,7 @@ int main() {
     world.add(std::make_shared<Sphere>(Point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(std::make_shared<Sphere>(Point3( 0.0,    0.0, -1.0),   0.5, material_center));
     world.add(std::make_shared<Sphere>(Point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(std::make_shared<Sphere>(Point3(-1.0,    0.0, -1.0),  -0.4, material_left));
     world.add(std::make_shared<Sphere>(Point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
